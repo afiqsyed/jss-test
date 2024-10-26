bfc64b791743ea3d46a2271dbb645103069bb720
afiqsyed <afiqsyed@gmail.com>
2024-10-27 02:01:26 +0800

    update powershell and yml

--- test.ps1
+++ test.ps1
@@ -13,11 +13,15 @@ Notes:
     5. It's a plus if you decide to use a testing framework that you're least familiar with.
 #>
  
+
+param (
+    [int]$Workers = 2,
+    [int]$Retries = 1
+)
+
 $projectRoot = (Get-Item $PSScriptRoot).parent.FullName
 $sampleAppPath = "$projectRoot/sample/nextjs-app"
 $url="http://localhost:3000"
- 
- 
 #Create Sitecore JSS application
 npx --yes create-sitecore-jss --templates nextjs,nextjs-styleguide --fetchWith REST --destination $sampleAppPath --yes
 
@@ -34,17 +38,16 @@ Get-ChildItem -Path $sampleAppPath | ForEach-Object {
     Write-Host $_.FullName
 }
 
-
 try {
     #Run the Sitecore JSS app in Disconnected mode. This will spin up the SUT in http://localhost:3000
     Write-Host "Running the Next.js Application in Disconnected mode..."
     $job = Start-Job -Name "RenderingHostJob_Disconnected" -InputObject $sampleAppPath -ScriptBlock {
+        param ($input)
         Set-Location $input
         $startOutput = npm run start 2>&1 | Out-String
         return $startOutput
     }
 
- 
     #Verify Site Availability
     $intervalSec=10
     $retries=10
@@ -52,8 +55,8 @@ try {
     $client = New-Object System.Net.WebClient
     do { Start-Sleep -Seconds $intervalSec; $retries-- } 
     until (
-        $(try { Write-Host "[$($originalRetryCount - $retries)] Requesting: $Url"
-            $response = $client.DownloadString($Url)
+        $(try { Write-Host "[$($originalRetryCount - $retries)] Requesting: $url"
+            $response = $client.DownloadString("$url")
             Write-Host "Response code: OK"
             $null -ne $response
         } catch [System.Net.WebException] { 
@@ -65,20 +68,18 @@ try {
     $jobOutput = Receive-Job -Job $job -Keep
     Write-Host "npm run start output:"
     Write-Host $jobOutput
- 
+
     #Run tests here
     Write-Output "Current Directory: $(Get-Location)"
     npm install
     npx playwright install --with-deps
-    npx playwright test
+    npx playwright test --workers=$Workers --retries=$Retries
 
     #Failed the pipeline if there are tests failed
     if ($LASTEXITCODE -ne 0) {
         Write-Error "Tests failed"
         exit 1
     }
-    
-    
 }
 finally {
     # Clean up any background jobs and node processes
